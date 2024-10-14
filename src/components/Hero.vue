a906e5720e021085e3d083ba03caf0a966a8ea8c
mariamtamerr <memariam5@outlook.com>
2024-10-14 17:17:46 +0300

    coountryFood

---
+++ src/components/Hero.vue
@@ -0,0 +1,69 @@
+<script setup></script>
+
+<template>
+  <div class="bgImg bg-black ">
+    <!-- Hero -->
+    <div class="">
+      <div class="mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8">
+        <!-- Title -->
+        <div class="max-w-2xl  text-center mx-auto">
+          <h1 class="block font-bold text-gray-200 text-4xl sm:text-5xl">
+            شريكك في بناء
+            <span class="text-[#FFCE42] font-bold text-4xl sm:text-5xl">
+              منصة توصيل
+            </span>
+
+            طعام متكاملة
+          </h1>
+          <p class="text-lg mt-5 mb-60 sm:mb-36 md:mb-10 text-white/70">
+            إذا كنت تمتلك فكرة لإنشاء تطبيق يوصل الأطعمة إلى العملاء أو ترغب في
+            إطلاق مشروع جديد يشبه التطبيقات الشهيرة مثل هانجر ستيشن، نحن هنا
+            لنساعدك على تحويل فكرتك إلى واقع بأعلى معايير الجودة.
+          </p>
+        </div>
+        <!-- End Title -->
+
+
+
+
+      <!-- start : images -->
+       <div class=" parent  flex justify-center gap-5 ">
+            <!-- <div class="one mt-44 -ml-16">
+                <img src="../assets/images/Your image.png" alt="screen"/>
+            </div>
+            <div class="hand  relative top-24">
+                <img class="opacity-50 " src="../assets/images/Background.png" alt="Handin Holding Screen"/>
+                <div class="absolute sm:size-36 md:size-36  size-24 md:top-8 sm:top-5 top-4 left-8 md:left-14 sm:left-12">
+                  <img class="" src="../assets/images/Your image.png" alt="screen"/>
+                </div>
+            </div> -->
+            <img class=" w-full -mb-24" src="../assets/images/screens4.png" />
+            <!-- <div class="three mt-28 ">
+                <img class="" src="../assets/images/Your image.png" alt="screen"/>
+            </div>      -->
+       </div>
+      <!-- end : images -->
+
+
+     
+
+
+
+      </div>
+    </div>
+    <!-- End Hero -->
+  </div>
+</template>
+
+<style scoped>
+.bgImg {
+  /* background-image: url('../assets/images/mexican-dishes-black-background 1.png'); */
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  /* height: 100vh;
+  width: 100%; */
+  
+
+}
+</style>
